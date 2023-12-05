9796b83f21d768efb04b3f9ba4163f39535c4d89
Yshrael <suporte2@brasolucoes.com>
2023-12-05 17:45:07 -0300

    add id in tag

--- web/src/components/Tag/index.js
+++ web/src/components/Tag/index.js
@@ -1,8 +1,8 @@
 import './index.css'
 import { isColorLight } from '../../utils/isColorLight'
 
-export function Tag({background}) {
-    return <div className={`Tag ${isColorLight(background)? 'contraste' : ''}`} style={{ background: background }}>
+export function Tag({background, id}) {
+    return <div id={id} className={`Tag ${isColorLight(background)? 'contraste' : ''}`} style={{ background: background }}>
         <div>Label</div> <div>100</div>
     </div>
 }
