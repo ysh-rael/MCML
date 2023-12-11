69105ead918ffffd51f2808ffadd4f79916d5006
Yshrael <suporte2@brasolucoes.com>
2023-12-11 15:44:25 -0300

    funcionou, porraaaa!

--- web/src/components/Fork/index.js
+++ web/src/components/Fork/index.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './index.css';
 import { designs } from './constants';
-import { useDrawImg, useFork } from './hooks';
+import { useDrawImg } from './hooks';
 import { inIsOnVertex } from '../../utils/inIsOnVertex';
 
 export function Fork({ img }) {
@@ -9,41 +9,46 @@ export function Fork({ img }) {
     const [Designs, setDesigns] = useState(designs);
     const [IsDragging, setIsDragging] = useState(null);
 
-    useDrawImg({ img, Designs, setDesigns, canvasRef })
-
-    // useFork({ canvasRef, Designs, a, setA })
+    useDrawImg({ img, Designs, setDesigns, canvasRef });
 
     useEffect(() => {
         const canvas = canvasRef.current;
 
-        canvas.addEventListener('mousedown', event => {
+        const handleMouseDown = (event) => {
             const { clientX: mouseX, clientY: mouseY } = event;
-
             const id = inIsOnVertex({ Designs, mouseX, mouseY, canvasRef });
-            setIsDragging(id)
-        });
+            setIsDragging(id);
+        };
 
-        canvas.addEventListener('mousemove', event => {
+        const handleMouseMove = (event) => {
             if (IsDragging) {
-                console.log(IsDragging)
-                //this._desenharCanvas();
+                console.log(IsDragging);
                 const x = event.clientX - canvasRef.current.getBoundingClientRect().left;
                 const y = event.clientY - canvasRef.current.getBoundingClientRect().top;
 
                 if (x || y) {
-                    // setIsDragging(prevIsDragging => {
-                    //     const updatedDesigns = [...Designs];
-                    //     updatedDesigns[prevIsDragging].x = x;
-                    //     updatedDesigns[prevIsDragging].y = y;
-                    //     return prevIsDragging;
-                    // });
+                    // Faça algo com x e y se necessário
                 }
             }
-        });
+        };
 
-        canvas.addEventListener('mouseup', () => setIsDragging(null));
-        canvas.addEventListener('mouseleave', () => setIsDragging(null));
-    }, [IsDragging, canvasRef])
+        const handleMouseUp = () => setIsDragging(null);
+        const handleMouseLeave = () => setIsDragging(null);
+
+        // Adiciona os event listeners
+        canvas.addEventListener('mousedown', handleMouseDown);
+        canvas.addEventListener('mousemove', handleMouseMove);
+        canvas.addEventListener('mouseup', handleMouseUp);
+        canvas.addEventListener('mouseleave', handleMouseLeave);
+
+        // Remove os event listeners quando o componente é desmontado
+        return () => {
+            canvas.removeEventListener('mousedown', handleMouseDown);
+            canvas.removeEventListener('mousemove', handleMouseMove);
+            canvas.removeEventListener('mouseup', handleMouseUp);
+            canvas.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }, [IsDragging, canvasRef]);
 
     return <canvas ref={canvasRef} className="Fork" id="c1" />;
 }
