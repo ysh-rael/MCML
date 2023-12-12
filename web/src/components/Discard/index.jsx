e8fd9f8b82762a424bebd530c958f9b8c91f4ec2
Yshrael <suporte2@brasolucoes.com>
2023-12-12 14:00:17 -0300

    fix discart

--- web/src/components/Discard/index.jsx
+++ web/src/components/Discard/index.jsx
@@ -1,8 +1,6 @@
 import './index.css'
 export function Discard({ ImgIndex, setImgIndex }) {
-    return <div className="Discard is-danger" onClick={() => {
-        setImgIndex(ImgIndex++)
-    }}>
+    return <div className="Discard is-danger" onClick={() => setImgIndex(ImgIndex + 1)}>
         <i className='fa-solid fa-trash'></i>
     </div>
 } 
