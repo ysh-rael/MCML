f89511077c98c5074516c5dc503064603e5bb9df
Yshrael <suporte2@brasolucoes.com>
2023-12-12 14:59:52 -0300

    refactoring

--- web/src/components/App/handler/index.js
+++ web/src/components/App/handler/index.js
@@ -2,7 +2,16 @@ import { closeModal } from "../../Modal/handler";
 
 export function handlerKeyPress({ event, setModalActive, setModelContent, ModalActive }) {
     try {
-        if (event.key && event.key === 'Escape' && ModalActive) closeModal({ setModalActive, setModelContent })
+
+        if (!event.key) return;
+        switch (event.key) {
+            case 'Escape':
+                if (ModalActive) closeModal({ setModalActive, setModelContent })
+                break;
+            default:
+                break;
+        }
+
     } catch (err) {
         console.error('err in handlerKeyPress: ' + err)
         console.error(err)
