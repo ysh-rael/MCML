737465d9ea7eab11a8deaaf8c6c50477e327e0b5
Yshrael <suporte2@brasolucoes.com>
2023-12-14 11:36:33 -0300

    backend router default print

--- backend/controller/default.js
+++ backend/controller/default.js
@@ -1,9 +1,11 @@
+const { Print } = require('../utils/print');
+const print = new Print({ informa: 'Controller Default', alerta: 'Controller Default', erro: 'Controller Default', sucesso: 'Controller Default' });
 function getDefault(req, res) {
     res.send('Pong');
 }
 
 function postDefault(req, res) {
-    console.log('req.body');
+    print.informa('req.body');
     console.log(req.body);
     res.send({ data: 'Pong', ok: true });
 }
