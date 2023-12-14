b098d9f49aabedd869475e5e45a1f06d6ee4ffdc
Yshrael <suporte2@brasolucoes.com>
2023-12-14 16:23:57 -0300

    temp

--- backend/controller/mcml/index.js
+++ backend/controller/mcml/index.js
@@ -18,7 +18,7 @@ async function loadAndPreprocessImage(imagePath) {
 
 async function prepareData() {
     // Definir rótulos (1 para "pessoa presente")
-    const labels = tf.tensor2d([[1]]);
+    const labels = tf.tensor2d([[5]]);
 
     // Carregar e pré-processar a imagem
     const imagePath = 'C:\\Users\\yshaeldev\\Desktop\\yshrael\\JS\\create-model-for-machine-learning\\backend\\images\\test.jpg';
@@ -53,37 +53,57 @@ async function saveModel(model) {
     await model.save('file://./trained-model');
 }
 
+
+
+
+
 async function loadModel() {
     const model = await tf.loadLayersModel('file://./trained-model/model.json');
     return model;
 }
 
-// Exemplo de uso
-async function useModel() {
-    const loadedModel = await loadModel();
-
-    // Agora você pode usar `loadedModel` para fazer previsões ou outras operações.
+async function loadAndPreprocessImage(imagePath) {
+    const imageTensor = tf.node.decodeImage(await fs.readFile(imagePath));
+    const expandedImage = imageTensor.expandDims(0); // Adicionar a dimensão de lote
+    const resizedImage = tf.image.resizeBilinear(expandedImage, [300, 300]);
+    return resizedImage;
 }
 
-
+async function predict(model, image) {
+    const prediction = model.predict(image);
+    // Processar a previsão conforme necessário
+    console.log('Prediction:', prediction.dataSync());
+}
 
 
 async function mcml(req, res) {
     try {
 
-        // Preparar os dados de treinamento
-        const { images, labels } = await prepareData();
+        if (!req) {
+            // Preparar os dados de treinamento
+            const { images, labels } = await prepareData();
 
-        // Criar e treinar o modelo
-        const model = await createAndTrainModel(images, labels);
+            // Criar e treinar o modelo
+            const model = await createAndTrainModel(images, labels);
 
-        // Salvar o modelo
-        await saveModel(model);
+            // Salvar o modelo
+            await saveModel(model);
 
-        // Chame a função para usar o modelo carregado
-        //useModel();
+            res.send({ data: 'Pong', ok: true });
+        } else {
+            // Caminho para a nova imagem
+            const imagePath = 'C:\\Users\\yshaeldev\\Desktop\\yshrael\\JS\\create-model-for-machine-learning\\backend\\images\\test.jpg';
 
-        res.send({ data: 'Pong', ok: true });
+            // Carregar o modelo
+            loadModel().then(async (model) => {
+                // Carregar e pré-processar a nova imagem
+                const image = await loadAndPreprocessImage(imagePath);
+
+                // Fazer a previsão
+                predict(model, image);
+            });
+        }
+
     } catch (error) {
         print.erro('error catch');
         console.error(error);
