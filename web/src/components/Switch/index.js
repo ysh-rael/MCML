84bcc51c48bccb64301ca1a79fe493669803b9e1
Ysh-rael <Ysp.rael@gmail.com>
2023-12-21 23:11:59 -0300

    init compnent Switch

---
+++ web/src/components/Switch/index.js
@@ -0,0 +1,6 @@
+export function Switch(params) {
+    return <div class="Switch">
+        <input type="checkbox" id="switchCheckbox"/>
+            <label for="switchCheckbox"></label>
+    </div>
+}
